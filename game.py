f7f31e7e4872bdd72dfd8c0f16ec5677795081f0
hubba <hubba@hubba.com>
2024-06-03 16:49:54 -0700

    Added random num

--- game.py
+++ game.py
@@ -1,5 +1,20 @@
 """A number-guessing game."""
 
+from random import randrange
+
+# greet player
 print("hello there!")
 
+# get player name
 name = input("\nWhat's your name? ")
+
+# choose random number between 1 and 100
+random_num = randrange(1, 100)
+
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
