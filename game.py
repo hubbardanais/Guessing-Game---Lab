d392650b256b8c8f5dd2bc0bc00a0b59b1ef0593
hubba <hubba@hubba.com>
2024-06-03 17:36:26 -0700

    Included user error catches

--- game.py
+++ game.py
@@ -2,34 +2,33 @@
 
 from random import randrange
 
-# greet player
+
 print("hello there!")
 
-# get player name
 name = input("\nWhat's your name? ")
 
-# choose random number between 1 and 100
 random_num = randrange(1, 100)
 
 guess_counter = 0
 
-# repeat forever:
 while True:
     guess = input(f"\nAlrighty, {name}! Guess a number! ")
-    guess = int(guess)
-#     get guess
-#     if guess is incorrect:
+    try:
+        guess = int(guess)
+    except ValueError:
+        print("\nYou have committed a crime. Guess a *number* between 1 and 100!")
+        continue
+    
     if guess != random_num:
-        if guess > random_num:
+        if guess > 100 or guess < 1:
+            print("\nYou have committed a crime. Guess a number between 1 and 100!")
+        elif guess > random_num:
             print(f"\n{guess} is too high! Try again!")
-        if guess < random_num:
+        elif guess < random_num:
             print(f"\n{guess} is too low! Try again!")
         guess_counter += 1
-
+        
     else:
         print(f"\nCongratulations, you won in only {guess_counter} tries!")
         break
-#         give hint
-#         increase number of guesses
-#     else:
-#         congratulate player
+        
